b5396976f4f3f7aacdc1a0c62b610460e07e936d
Gokcem Kaplan <gkaplan@ripe.net>
2023-04-10 18:48:42 +0200

    Add first components and routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {TripListComponent} from './trip-list/trip-list.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'trip-list', component: TripListComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+  {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    TripListComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    [RouterModule.forRoot(routes)]
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
